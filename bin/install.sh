fb6035c60acafd0a035b44addfb8ce42b68a30d2
Yuma Ishikawa <yuma.fuu05@gmail.com>
2020-09-30 15:15:16 +0900

    [fix] bin

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 path="$HOME/.config/nvim"
 mkdir -p $path
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/wezterm.lua > $path/wezterm.lua
+curl https://raw.githubusercontent.com/YumaFuu/wezterm/master/wezterm.lua > $path/wezterm.lua
 
 echo done
 
